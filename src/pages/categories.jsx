96decdee0c65617749e8ad93f4692e97fcc3f956
VLADYSLAVA KLOVAK <vladaklovak@gmail.com>
2022-11-02 15:10:50 +0100

    pages and components created

---
+++ src/pages/categories.jsx
@@ -0,0 +1,14 @@
+export const Categories = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <b>file name:</b> categories.jsx
+        </li>
+        <li>
+          <b>component name:</b> Categories
+        </li>
+      </ul>
+    </div>
+  );
+};
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <b>file name:</b> home.jsx
+        </li>
+        <li>
+          <b>component name:</b> Home
+        </li>
+      </ul>
+    </div>
+  );
+};
